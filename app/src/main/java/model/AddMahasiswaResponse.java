2c84265501929247975f516de7dd6f5a8482cd54
Sari <sari.142002@gmail.com>
2022-12-02 20:43:37 +0700

    first commit

---
+++ app/src/main/java/model/AddMahasiswaResponse.java
@@ -0,0 +1,16 @@
+package model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class AddMahasiswaResponse {
+    @SerializedName("message")
+    private String message;
+    @SerializedName("status")
+    private boolean status;
+    public String getMessage(){
+        return message;
+    }
+    public boolean isStatus(){
+        return status;
+    }
+}
